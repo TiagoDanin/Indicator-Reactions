cb6d79f06520f40d466f0a400e92f661576193f5
TiagoDanin <Tiagojdanin@outlook.com>
2016-07-20 16:47:06 -0500

    ADD: Setup.py, New reaction & Git-ignore

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+# coding: utf-8
+# setup.py
+
+from distutils.core import setup
+
+setup(name='IndicatorReactions',
+		version ='0.2',
+		description = 'Simple indicator with Reactions ¯ \ _ (ツ) _/¯',
+		long_description = '''
+		''',
+		author = 'Tiago Danin',
+		author_email = 'TiagoDanin@outlook.com',
+		license = 'GPLv3',
+		url = 'https://TiagoDanin.github.io/Indicator-Reactions',
+		scripts = ['indicator-reactions'],
+		classifiers = [
+			'Development Status :: 4 - Beta',
+			'Operating System :: Unix',
+			'Programming Language :: Python :: 2.7',
+			'Topic :: Utilities',
+		],
+		keywords = '')
